885ca0fddbf1ab6366b86f6c83d9e9a3323c9d97
odv144 <omar.virili@gmail.com>
2023-11-10 15:35:02 -0300

    subiendo proyecto unidad 3

---
+++ src/Components/Section/Section.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Link } from '@chakra-ui/react'
+
+const Section = () => {
+  return (
+    <div>
+        <Box as='header' bg='grey'>
+           <Link href='www.gogole.com'>Estoy probando</Link>
+        </Box>
+    </div>
+  )
+}
+
+export default Section
